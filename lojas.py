01b1b945b26e6ade34d7b968fbf696ff64cd9e19
ygor <152539555+xooxq@users.noreply.github.com>
2024-06-08 21:44:07 -0300

    lojas.py

--- lojas.py
+++ lojas.py
@@ -9,17 +9,19 @@ if get("https://www.kabum.com.br/produto/161613/processador-amd-ryzen-5-3600x-3-
 
 else:
     #item = input("Item: ").strip().capitalize()
-    html = BeautifulSoup(get("https://www.kabum.com.br/produto/165133/placa-mae-asus-tuf-gaming-a520m-plus-ii-amd-am4-matx-ddr4").text, "html.parser")
+    html = BeautifulSoup(get("https://www.kabum.com.br/produto/161613/processador-amd-ryzen-5-3600x-3-8ghz-4-4ghz-max-turbo-cache-32mb-am4-ddr4-100-100000022box").text, "html.parser")
     card=html.find("div", id="blocoValores")
-    nome = html.find('h1', {'class':['sc-58b2114e-6', 'brTtKt']}).getText()
+    nome_produto = html.find('h1', {'class':['sc-58b2114e-6', 'brTtKt']}).getText()
     vendedor = html.find('div', {'class': ['sc-477542eb-1', 'YqEBe']}).get_text().split('|')[0]
     em_estoque=html.find('div', {'class': ['sc-477542eb-1', 'YqEBe']}).get_text().split('|')[1].strip()
     preco_pix = html.find("h4", {'class':['sc-5492faee-2','ipHrwP','finalPrice']}).get_text()
     msg_pix = html.find('span', {'class': ['sc-5492faee-3', 'igKOYC']}).get_text()
     valor_real = html.find('b', {'class': 'regularPrice'}).get_text()
     msg_parcelamento = html.find('span', class_="cardParcels").get_text() + "\n" + card.findAll('span')[-1].getText()
+    titulo_produto = html.find('button', id='radix-:R12jcc6:').getText().strip()
+    descricao_produto = html.find('div', id='radix-:R32jcc6:').getText()
     img_produto = html.find('meta', property="og:image")["content"]
-
-    print(f"{nome}\n\n{vendedor}\nEstado: {em_estoque}\nPreço: {preco_pix}\n{msg_pix}\n\n{valor_real}\n{msg_parcelamento}")
     
-    #pegar a descrição do produto
+    print(f"{nome_produto}\n\n{vendedor}\nEstado: {em_estoque}\nPreço: {preco_pix}\n{msg_pix}\n\n{valor_real}\n{msg_parcelamento}\n\n{titulo_produto}\n\n{descricao_produto}")
+
+    
